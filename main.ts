f8080fbea6240138ba634c819b273648487779a5
yus840332 <70998708+yus840332@users.noreply.github.com>
2020-09-09 14:31:01 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,25 @@
-
+basic.forever(function () {
+    basic.showString("The5970")
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Yes)
+    basic.showIcon(IconNames.No)
+    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Sad)
+    basic.showIcon(IconNames.Confused)
+    basic.showIcon(IconNames.Angry)
+    basic.showIcon(IconNames.Asleep)
+    basic.showIcon(IconNames.Surprised)
+    basic.showIcon(IconNames.Silly)
+    basic.showIcon(IconNames.Fabulous)
+    basic.showIcon(IconNames.Meh)
+    basic.showIcon(IconNames.TShirt)
+    basic.showIcon(IconNames.Rollerskate)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+})
